a61721b606d61cc30a43f3b3e65b3062f00d294b
tokeyjs <tokeyjs@qq.com>
2024-08-18 19:15:04 +0800

    折射

--- LearnOpenGL/cube.fs
+++ LearnOpenGL/cube.fs
@@ -9,7 +9,8 @@ uniform vec3 cameraPos;
 
 void main()
 {    
+    float ratio = 1.00 / 1.52;
     vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+    vec3 R = refract(I, normalize(Normal), ratio);
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
